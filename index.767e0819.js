62f46d54fbff4e0e01f6e804a181cb854c3193b0
Stas Borychevskyi <sborichevskiy74@gmail.com>
2024-09-30 18:23:13 +0300

    make build

---
+++ index.767e0819.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=getComputedStyle(document.querySelector(".wall")),p=getComputedStyle(e),r=.5*Number(t.width.replace("px",""))-.5*Number(p.width.replace("px","")),l=.5*Number(t.height.replace("px",""))-.5*Number(p.height.replace("px",""));e.style.left="".concat(r,"px"),e.style.top="".concat(l,"px");
+//# sourceMappingURL=index.767e0819.js.map
